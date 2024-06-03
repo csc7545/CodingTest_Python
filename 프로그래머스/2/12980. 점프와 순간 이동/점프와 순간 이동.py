1255c43cea8de94c333fdf1cd169e1e9731a3803
Unduck <74640695+csc7545@users.noreply.github.com>
2024-06-03 15:04:49 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -0,0 +1,13 @@
+def solution(N):
+    battery_usage = 0
+    while N > 0:
+        if N % 2 == 0:
+            N //= 2
+        else:
+            N -= 1
+            battery_usage += 1
+    return battery_usage
+
+if __name__ == "__main__":
+    N = int(input())
+    print(solution(N))
